434c5a6eb2fb201044bb87021df2741cf92b927d
n54hayden <156018512+n54hayden@users.noreply.github.com>
2025-10-04 15:25:18 -0700

    workingapp

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,10 @@ module.exports = {
     }
   },
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }
+    ]
   }
 };
