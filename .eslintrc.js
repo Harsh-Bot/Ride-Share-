8fd9864b1eb387c81094434cabea4862dcbf7213
dhara <dharapatel4555@gmail.com>
2025-10-05 08:52:30 -0700

    updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,8 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': [
       'warn',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }
-    ]
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-empty': ['warn', { allowEmptyCatch: true }]
   }
 };
--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import type { ExpoConfig } from '@expo/config';
 import 'dotenv/config';
 
